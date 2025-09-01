00991fe0ba525f9e01c598f9611a3b355a4de159
RodrigoDosSantos <rodrigosantospx@gmail.com>
2025-09-01 17:28:34 -0300

    :tada: inicio do projeto

---
+++ backTeuServico/src/main/java/com/teuServico/backTeuServico/BackTeuServicoApplication.java
@@ -0,0 +1,13 @@
+package com.teuServico.backTeuServico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTeuServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackTeuServicoApplication.class, args);
+	}
+
+}
---
+++ backTeuServico/src/main/java/com/teuServico/backTeuServico/services/exceptions/BusinessException.java
@@ -0,0 +1,13 @@
+package com.teuServico.backTeuServico.services.exceptions;
+
+public class BusinessException extends  RuntimeException{
+
+    public BusinessException(String message) { // retorna uma mensagem simples
+        super(message);
+    }
+
+    public BusinessException(String message, Object ... params) { // retorna uma mensagem customizada atraves do varargs
+        super(String.format(message, params));
+    }
+
+}
