812831bb8474e190bbe3108127c888c81ebcb17f
RodrigoDosSantos <rodrigosantospx@gmail.com>
2025-10-07 19:39:07 -0300

    feat: add validacoes a classe endereco

--- backTeuServico/src/main/java/com/teuServico/backTeuServico/appUsuarios/model/Endereco.java
+++ backTeuServico/src/main/java/com/teuServico/backTeuServico/appUsuarios/model/Endereco.java
@@ -2,19 +2,38 @@ package com.teuServico.backTeuServico.appUsuarios.model;
 
 import com.teuServico.backTeuServico.appUsuarios.model.enums.EstadoEnum;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Embeddable
 public class Endereco {
+    @NotBlank(message = "rua é inválido")
+    @Size(max = 100, message = "rua deve ter no máximo 100 caracteres")
     private String rua;
+
+    @NotBlank(message = "numero é inválido")
+    @Size(max = 10, message = "numero deve ter no máximo 5 caracteres")
     private String numero;
+
+    @Size(max = 50, message = "complemento deve ter no máximo 50 caracteres")
     private String complemento;
+
+    @NotBlank(message = "bairro é inválido")
+    @Size(max = 100, message = "bairro deve ter no máximo 100 caracteres")
     private String bairro;
+
+    @NotBlank(message = "cidade é inválido")
+    @Size(max = 100, message = "cidade deve ter no máximo 100 caracteres")
     private String cidade;
 
+    @NotNull(message = "estado é inválido")
     @Enumerated(EnumType.STRING)
     private EstadoEnum estado;
 
+    @NotBlank(message = "cep é inválido")
     private String cep;
+
 
     public Endereco() {
     }
