f92397be7297f7ac41db53af029ec4e3f5b42eee
RodrigoDosSantos <rodrigosantospx@gmail.com>
2025-10-14 16:05:07 -0300

    feat: renomeando listarEntidades para listarTodos e criando metodo de Paginacao com um metodoDeBusca generico

--- backTeuServico/src/main/java/com/teuServico/backTeuServico/shared/utils/Paginacao.java
+++ backTeuServico/src/main/java/com/teuServico/backTeuServico/shared/utils/Paginacao.java
@@ -29,16 +29,43 @@ public class Paginacao {
      * @return Um objeto {@link PaginacaoResponseDTO} contendo os dados paginados
      * @throws BusinessException Se o número da página for maior que o total de páginas disponíveis.
      */
-    public <T, R, ID> PaginacaoResponseDTO<R> listarEntidades(int pagina, int qtdMaximaDeElementos, JpaRepository<T, ID> repository, Function<T, R> mapper, Sort sort) {
+    public <T, R, ID> PaginacaoResponseDTO<R> listarTodos(int pagina, int qtdMaximaDeElementos, JpaRepository<T, ID> repository, Function<T, R> mapper, Sort sort) {
         int numeroPagina= pagina;
         numeroPagina = Math.max(0, numeroPagina - 1);
         Pageable pageable = PageRequest.of(numeroPagina, qtdMaximaDeElementos, sort);
         Page<T> page = repository.findAll(pageable);
-        if (numeroPagina >= page.getTotalPages()) {
+        if (page.getTotalPages() >= 1 && numeroPagina >= page.getTotalPages()) {
             throw new BusinessException("O número da página não pode ser maior que o total de páginas.");
         }
         List<R> conteudo = page.getContent().stream().map(mapper).collect(Collectors.toList());
         return new PaginacaoResponseDTO<>(page.getTotalElements(), page.getTotalPages(), page.getNumber(), conteudo);
     }
+
+
+    /**
+     * Lista entidades de forma paginada a partir de um metodo de busca Pageable -> Page
+     *
+     * @param <T> Tipo da entidade original.
+     * @param <R> Tipo mapeado para retorno(ReponseDTO)
+     * @param pagina Número da página solicitada (base 1).
+     * @param qtdMaximaDeElementos Numeros de elementos que estarao presentes em um página
+     * @param metodoDeBusca função que recebe um {@link Pageable} e retorna uma {@link Page} de elementos do tipo {@code T}
+     * @param mapper Função de mapeamento da entidade original para o tipo de retorno(ReponseDTO) do conteudo
+     * @param sort Critério de ordenação a ser aplicado.
+     * @return Um objeto {@link PaginacaoResponseDTO} contendo os dados paginados
+     * @throws BusinessException Se o número da página for maior que o total de páginas disponíveis.
+     */
+
+    public <T, R> PaginacaoResponseDTO<R> listarPor(int pagina, int qtdMaximaDeElementos, Function<Pageable, Page<T>> metodoDeBusca, Function<T, R> mapper, Sort sort) {
+        int numeroPagina = Math.max(0, pagina - 1);
+        Pageable pageable = PageRequest.of(numeroPagina, qtdMaximaDeElementos, sort);
+        Page<T> page = metodoDeBusca.apply(pageable);
+        if (page.getTotalPages() >= 1 && numeroPagina >= page.getTotalPages()) {
+            throw new BusinessException("O número da página não pode ser maior que o total de páginas.");
+        }
+        List<R> conteudo = page.getContent().stream().map(mapper).collect(Collectors.toList());
+        return new PaginacaoResponseDTO<>(page.getTotalElements(), page.getTotalPages(), page.getNumber(), conteudo);
+    }
+
 
 }
